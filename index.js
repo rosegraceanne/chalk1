b5d7c53e1be680b0f210965724f7683c82aff75f
Rose <rhaneyrrn@gmail.com>
2018-10-28 15:46:08 -0500

    Used chalk

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello!'));
+console.log(chalk.red('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas hendrerit nibh quis mattis pharetra. Sed tempor bibendum nisi non feugiat. Curabitur consectetur felis purus, quis convallis mauris luctus vel. Nunc ac porta neque, eu faucibus neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec quis lacus dui. Vestibulum massa nulla, dictum vitae lobortis sed, cursus id elit. Suspendisse pellentesque ornare lectus, nec posuere nibh aliquam eu. Praesent aliquam aliquet nibh eu pellentesque. Etiam elementum, ipsum vel condimentum pretium, diam nibh iaculis diam, in ultrices ligula tortor quis nulla. Fusce at felis sit amet nunc vestibulum consectetur id porta neque. Nullam tortor ex, faucibus auctor accumsan ut, mollis sit amet magna.'));
+
